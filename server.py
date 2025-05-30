d55f4738c450eef1dfd198b41b0c679814e86baf
Stefano Marrone <stefano.marrone@unicampania.it>
2025-05-30 09:52:51 +0200

    very small change

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ router = APIRouter()
 @router.get("/getbeat")
 def beat():
    current_bpm = random.randrange(50, 141)  #BPM tra 50 e 140 battiti
-   #il secondo valore è escluso, quindi 141 per includere 140
+   #il secondo valore escluso, quindi 141 per includere 140
    return {'beat': current_bpm}
 
 app = FastAPI()
